ca041ee797ce081e98be9f1fd08a39c4f5d0eee4
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2017-02-24 00:18:30 +0545

    connection pool

--- db.js
+++ db.js
@@ -11,7 +11,12 @@ module.exports = app => {
 			config.database,
 			config.username,
 			config.password,
-			config.params
+			config.params,
+			 pool: {
+    max: 5,
+    min: 0,
+    idle: 30000
+  }
 		);
 		db = {
 			sequelize,
