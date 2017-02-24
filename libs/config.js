9d57339df4f1ccf3e5bce35ee1b01369e1da80f4
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2017-02-24 11:21:30 +0545

    db port

--- libs/config.js
+++ libs/config.js
@@ -5,7 +5,6 @@ module.exports = {
 	password: "2bdf3c35",
 	params: {
 		host: "us-cdbr-iron-east-04.cleardb.net",
-		port: process.env.PORT || 3000,
 		dialect: "mysql",
 		logging: (sql) => {
 			logger.info(`[${new Date()}] ${sql}`);
