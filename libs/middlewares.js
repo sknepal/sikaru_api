2f074d0e3f6a801f6d029424ca1f63b42272b55a
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2017-02-24 12:21:53 +0545

    dynamic port handling in heroku

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -8,7 +8,7 @@ import logger from "./logger.js";
 
 
 module.exports = app => {
-	app.set("port", 3000);
+	app.set("port", process.env.PORT || 3000);
 	app.set("json spaces", 4);
 	app.use(morgan("common", {
 		stream: {
