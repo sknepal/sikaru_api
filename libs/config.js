3a3b4fe17d4c009ed5fd3f407f382c3f8f496e2e
Subigya Nepal <subigya.nepal@deerwalk.edu.np>
2017-02-24 12:00:43 +0545

    reconnect

--- libs/config.js
+++ libs/config.js
@@ -1,10 +1,10 @@
 import logger from "./logger.js"
 module.exports = {
-	database: "heroku_736b203e2aca3eb?reconnect=true",
+	database: "heroku_736b203e2aca3eb",
 	username: "bd3ef237df9495",
 	password: "2bdf3c35",
 	params: {
-		host: "us-cdbr-iron-east-04.cleardb.net",
+		host: "us-cdbr-iron-east-04.cleardb.net?reconnect=true",
 		dialect: "mysql",
 		pool: {
 		    max: 5,
